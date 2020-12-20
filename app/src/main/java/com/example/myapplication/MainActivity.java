40c654a33af35df7b41d43e9d5cfc7bbc16f1a11
first commit <artem333stup@gmail.com>
2020-12-20 20:46:14 +0300

    second commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,48 +9,38 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView text;
-    Button summ;
-    EditText ed1,ed2,ed3;
-
-
+    TextView lblresult;
+    Button vhod;
+    EditText pass,log;
+     final String password = "1234";
+     final String login = "artem";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        summ = findViewById(R.id.sum);
 
-        text = findViewById(R.id.text);
-        ed1 = findViewById(R.id.number1);
-        ed2= findViewById(R.id.number2);
-        ed3= findViewById(R.id.number3);
-        summ.setOnClickListener(this);
+
+        lblresult = findViewById(R.id.text);
+       pass = findViewById(R.id.password);
+        log = findViewById(R.id.login);
+        vhod= findViewById(R.id.vhod);
+        vhod.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick (View v) {
-        String t1 = ed1.getText().toString();
-        String t2 = ed2.getText().toString();
-        String t3 = ed3.getText().toString();
-        double a = Integer.parseInt(t1);
-        double b = Integer.parseInt(t2);
-        double c = Integer.parseInt(t3);
-        if (a==0){
-            text.setText("Error");
+        String s1 = pass.getText().toString();
+        String s2 = log.getText().toString();
+        if (s1.equals(password) && s2.equals(login)){
+            lblresult.setText("Верно");
         }
-        double D = b*b - 4*c*a;
-        if (D<0){
-            text.setText("Error");
-        }
-        double x1 = ((-b+Math.sqrt(D))/(2*a));
-        double x2= ((-b-Math.sqrt(D))/(2*a));
-        if (D==0){
-            text.setText(String.valueOf(x1=-b/2*a));
-        } else{
-            text.setText(String.valueOf(x1) + " "+ " "+ String.valueOf(x2));
+        else {
+            lblresult.setText("Вы ошиблись в логине или пароле");
+            log.setText(null);
+            pass.setText (null);
         }
 
 
